7245c6c424184b4cb8901ba7e729d4bb5127ce3d
Nils Matthäi <nils.matthaei@gmail.com>
2023-01-28 12:09:46 +0100

    fixed line width calulation to be accurate for UFT-16 characters

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
 use std::env;
 use std::fs;
 use textwrap::fill;
+use unicode_width::UnicodeWidthStr;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let path = "asciiart/";                     //planned to be changeable via "--setpath" flag
+    let path = "asciiart/"; //planned to be changeable via "--setpath" flag
     let file = &args[1];
     let text = &args[2];
     let path_abs = path.to_owned() + &file + ".txt";
 
-    let asciiart = fs::read_to_string(path_abs)
-        .expect("Should have been able to read file");
-    
-    println!("{}",bubble(text));
-    println!("{}",asciiart);
+    let asciiart = fs::read_to_string(path_abs).expect("Should have been able to read file");
+
+    println!("{}", bubble(text));
+    println!("{}", asciiart);
 }
 
-
-fn bubble(input: &str) -> String  {
-    let dashes= "\n   \\ \n    \\";
+fn bubble(input: &str) -> String {
+    let dashes = "\n   \\ \n    \\";
     let line_limit = 50;
 
     let wrapped = fill(input, line_limit);
@@ -30,33 +29,34 @@ fn bubble(input: &str) -> String  {
 
     //Top Border
     let mut out: String = " ".to_string();
-    out = out + &("_".repeat(line_width+2))+" \n";
+    out = out + &("_".repeat(line_width + 2)) + " \n";
 
     //Text
-    for(i, line) in lines.into_iter().enumerate(){
-        if num_lines == 1{
-            out = out + "< "+ line + " >\n";
-
-        }else if i == 0 {
-            out = out + "/ "+ line + &(" ".repeat(line_width-line.len())) + " \\\n";
-        }else if i == num_lines -1 {
-            out = out + "\\ "+ line + &(" ".repeat(line_width-line.len())) + " /\n";
-        }
-        else {
-            out = out + "| "+ line + &(" ".repeat(line_width-line.len())) + " |\n";
+    for (i, line) in lines.into_iter().enumerate() {
+        let diff = line_width - UnicodeWidthStr::width(line);
+        if num_lines == 1 {
+            out = out + "< " + line + " >\n";
+        } else if i == 0 {
+            out = out + "/ " + line + &(" ".repeat(diff)) + " \\\n";
+        } else if i == num_lines - 1 {
+            out = out + "\\ " + line + &(" ".repeat(diff)) + " /\n";
+        } else {
+            out = out + "| " + line + &(" ".repeat(diff)) + " |\n";
         }
     }
-    out = out + " " + &("-".repeat(line_width+2));
+    out = out + " " + &("-".repeat(line_width + 2));
     out = out + dashes;
 
     return out;
 }
 
-fn widest_line(lines: &[&str]) -> usize{
-    let mut wideboy = lines[0].len();
-    for line in lines{
-        if line.len() > wideboy{
-            wideboy = line.len();
+fn widest_line(lines: &[&str]) -> usize {
+    let mut wideboy = UnicodeWidthStr::width(lines[0]);
+    let mut line_width;
+    for line in lines {
+        line_width = UnicodeWidthStr::width(line.to_owned());
+        if line_width > wideboy {
+            wideboy = line_width;
         }
     }
     return wideboy;
